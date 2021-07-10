67c4db6a1264f6b88e0fe86b12440fa3a1580a0c
Marc Ishak <marc_ishak@msn.com>
2021-07-10 15:02:55 +1000

    * Changed default split for html from chapter to sections
    * Swapped Ackowledgements And Abstract in html version

--- Build.R
+++ Build.R
@@ -1,4 +1,4 @@
-# Builds site with option to download pdf
+# Builds pdf and site -> enables option to download pdf
 
 bookdown::render_book("_output.yaml", "bookdown::pdf_book", output_dir = "docs")
 bookdown::render_book("_output.yaml", "bookdown::gitbook", output_dir = "docs")
